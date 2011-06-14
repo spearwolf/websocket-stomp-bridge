4612695aa6206ac073cc7e4a203147e4a9901d5a
Wolfger Schramm <wolfger@spearwolf.de>
2011-06-14 10:06:58 +0200

    updated gems; minor changes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "http://rubygems.org"
 
-gem "em-websocket", "~>0.1.4"
-gem "em-http-request", "~>0.2.14"
+gem "em-websocket", "~>0.3.0"     # "~>0.1.4"
+gem "em-http-request", "~>0.3.0"  # "~>0.2.14"
 
 # vim: ft=ruby
--- lib/ws_stomp_bridge/channel_manager.rb
+++ lib/ws_stomp_bridge/channel_manager.rb
@@ -6,7 +6,20 @@ module WsStompBridge
     extend self
 
     def channel(name)
-      (@channels ||= {})[name] ||= EM::Channel.new
+      # (@channels ||= {})[name] ||= EM::Channel.new
+      @channels ||= {}
+      if @channels[name].nil?
+        mutex.synchronize {
+          @channels[name] = EM::Channel.new
+        }
+      end
+      return @channels[name]
+    end
+    
+    private
+    
+    def mutex
+      @mutex ||= Mutex.new
     end
   end
 end
